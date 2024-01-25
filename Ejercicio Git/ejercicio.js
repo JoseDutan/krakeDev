ca98b49e96718132d98eb6611277c7a91efd8184
JoseDutan <josedutan86@gmail.com>
2024-01-25 11:10:08 -0500

    el boton modfica

--- Ejercicio Git/ejercicio.js
+++ Ejercicio Git/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById(imgSaludo);
+    cmpImagenSaludo.src="./Imagenes/Rotating_earth_(large).gif"
     console.log("Ingresar a Saludar");
+    
 }
