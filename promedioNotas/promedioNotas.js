a36a9d4e03aa1713a843b971b1bacd576b6d2be6
JoseDutan <josedutan86@gmail.com>
2024-01-31 21:15:00 -0500

    Ejercicio promedio notas version 2

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -11,10 +11,18 @@ calcularPromedioNotas=function(){
     promedio=calcularPromedio(nota1, nota2, nota3);
     mostrarTexto("lblPromedio", promedio);
     
-    if(promedio>7){
-        mostrarImagen("lblImagen", "./el-exito.gif")
-    }else if(promedio<7){
+    if(promedio > 0 && promedio< 5){
         mostrarImagen("lblImagen", "./donkey-kong-triste.gif")
+        mostrarTexto("lblMensaje", "REPROBADO");
+    }else if(promedio >= 5 && promedio <= 8){
+        mostrarImagen("lblImagen", "./el-exito.gif");
+        mostrarTexto("lblMensaje", "BUEN TRABAJO");
+    }else if(promedio > 8 && promedio <= 10){
+        mostrarImagen("lblImagen", "./excelente.gif");
+        mostrarTexto("lblMensaje", "EXCELENTE");
+    }else if(promedio < 0 || promedio > 10){
+        mostrarImagen("lblImagen", "./datosincorrectos.gif" )
+        mostrarTexto("lblMensaje", "DATOS INCORRECTOS");
     }
 
 }
