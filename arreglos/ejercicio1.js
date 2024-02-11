3e8fdc2784bebccd85f84d27fe79993e92808081
JoseDutan <josedutan86@gmail.com>
2024-02-10 19:49:03 -0500

    .

--- arreglos/ejercicio1.js
+++ arreglos/ejercicio1.js
@@ -22,6 +22,7 @@ probarAgregar = function () {
 
 agregarNota = function (nota) {
     notas.push(nota);
+    mostraNota();
 }
 
 calcularPromedio = function () {
@@ -39,4 +40,26 @@ ejecutarPromedio = function () {
     invocar = calcularPromedio();
     mostrarTexto("lblPromedio", invocar);
 
+}
+
+generarTabla=function(){
+    let contenidoTabla="";
+    let cmpTabla = document.getElementById("divTable");
+    contenidoTabla+="<table><tr><td>UNO</td></tr></table>"+
+    "<table><tr><td>DOS</td></tr></table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
+
+mostraNota=function(){
+    let cmpTabla = document.getElementById("divTable");
+    let contenidoTabla="<table><tr><th>NOTA</th></tr>";
+    let miNota
+    for(let i=0;i<notas.length;i++){
+        miNota=notas[i]
+        contenidoTabla+="<tr><td>";
+        contenidoTabla+=miNota;
+        contenidoTabla+="</tr></td>"
+    }
+    contenidoTabla+="</table>"
+    cmpTabla.innerHTML=contenidoTabla;
 }
